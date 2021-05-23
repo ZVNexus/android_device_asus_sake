eb40bb00426d8ebfa6e271047024250b703de502
Alexander Koskovich <zvnexus@outlook.com>
2021-05-22 23:15:59 -0700

    sake: Initial product definition.
    Change-Id: Icda96be5440169fd26f13a49922a14218dc3972d

---
+++ BoardConfig.mk
@@ -0,0 +1,224 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PATH := device/asus/sake
+
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor_boot
+
+# AVB
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += \
+    --set_hashtree_disabled_flag \
+    --flags 2
+
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+BOARD_AVB_VBMETA_SYSTEM := product system system_ext
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := lahaina
+
+# DTB
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+# DTBO
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+# Kernel
+BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    cgroup.memory=nokmem,nosocket \
+    console=ttyMSM0,115200n8 \
+    ip6table_raw.raw_before_defrag=1 \
+    iptable_raw.raw_before_defrag=1 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    pcie_ports=compat \
+    service_locator.enable=1 \
+    swiotlb=0 \
+    loop.max_part=7 \
+    video=vfb:640x400,bpp=32,memsize=3072000
+
+BOARD_DO_NOT_STRIP_VENDOR_MODULES := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_BINARIES := kernel kernel-gki
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_RAMDISK_USE_LZ4 := true
+
+BOARD_VENDOR_KERNEL_MODULES := \
+    $(KERNEL_MODULES_OUT)/adsp_loader_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/apr_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/aw8697.ko \
+    $(KERNEL_MODULES_OUT)/bolero_cdc_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
+    $(KERNEL_MODULES_OUT)/btpower.ko \
+    $(KERNEL_MODULES_OUT)/camera.ko \
+    $(KERNEL_MODULES_OUT)/cs35l45_i2c_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/e4000.ko \
+    $(KERNEL_MODULES_OUT)/fc0011.ko \
+    $(KERNEL_MODULES_OUT)/fc0012.ko \
+    $(KERNEL_MODULES_OUT)/fc0013.ko \
+    $(KERNEL_MODULES_OUT)/fc2580.ko \
+    $(KERNEL_MODULES_OUT)/focaltech_fts_zf.ko \
+    $(KERNEL_MODULES_OUT)/gf_spi.ko \
+    $(KERNEL_MODULES_OUT)/hdmi_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/it913x.ko \
+    $(KERNEL_MODULES_OUT)/lid.ko \
+    $(KERNEL_MODULES_OUT)/lid_2.ko \
+    $(KERNEL_MODULES_OUT)/llcc_perfmon.ko \
+    $(KERNEL_MODULES_OUT)/m88rs6000t.ko \
+    $(KERNEL_MODULES_OUT)/machine_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/max2165.ko \
+    $(KERNEL_MODULES_OUT)/mbhc_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/mc44s803.ko \
+    $(KERNEL_MODULES_OUT)/msi001.ko \
+    $(KERNEL_MODULES_OUT)/mt2060.ko \
+    $(KERNEL_MODULES_OUT)/mt2063.ko \
+    $(KERNEL_MODULES_OUT)/mt20xx.ko \
+    $(KERNEL_MODULES_OUT)/mt2131.ko \
+    $(KERNEL_MODULES_OUT)/mt2266.ko \
+    $(KERNEL_MODULES_OUT)/mxl301rf.ko \
+    $(KERNEL_MODULES_OUT)/mxl5005s.ko \
+    $(KERNEL_MODULES_OUT)/mxl5007t.ko \
+    $(KERNEL_MODULES_OUT)/native_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/pinctrl_lpi_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/pinctrl_wcd_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/platform_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/q6_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/q6_notifier_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/q6_pdr_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/qm1d1b0004.ko \
+    $(KERNEL_MODULES_OUT)/qm1d1c0042.ko \
+    $(KERNEL_MODULES_OUT)/qt1010.ko \
+    $(KERNEL_MODULES_OUT)/r820t.ko \
+    $(KERNEL_MODULES_OUT)/rdbg.ko \
+    $(KERNEL_MODULES_OUT)/rmnet_core.ko \
+    $(KERNEL_MODULES_OUT)/rmnet_ctl.ko \
+    $(KERNEL_MODULES_OUT)/rmnet_offload.ko \
+    $(KERNEL_MODULES_OUT)/rmnet_shs.ko \
+    $(KERNEL_MODULES_OUT)/rx_macro_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/si2157.ko \
+    $(KERNEL_MODULES_OUT)/sla.ko \
+    $(KERNEL_MODULES_OUT)/slimbus-ngd.ko \
+    $(KERNEL_MODULES_OUT)/slimbus.ko \
+    $(KERNEL_MODULES_OUT)/snd-soc-es928x.ko \
+    $(KERNEL_MODULES_OUT)/snd_event_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/stub_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/swr_ctrl_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/swr_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/swr_dmic_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/swr_haptics_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/sx932x_sake.ko \
+    $(KERNEL_MODULES_OUT)/tda18212.ko \
+    $(KERNEL_MODULES_OUT)/tda18218.ko \
+    $(KERNEL_MODULES_OUT)/tda18250.ko \
+    $(KERNEL_MODULES_OUT)/tda9887.ko \
+    $(KERNEL_MODULES_OUT)/tea5761.ko \
+    $(KERNEL_MODULES_OUT)/tea5767.ko \
+    $(KERNEL_MODULES_OUT)/texfat.ko \
+    $(KERNEL_MODULES_OUT)/tntfs.ko \
+    $(KERNEL_MODULES_OUT)/tua9001.ko \
+    $(KERNEL_MODULES_OUT)/tuner-simple.ko \
+    $(KERNEL_MODULES_OUT)/tuner-types.ko \
+    $(KERNEL_MODULES_OUT)/tuner-xc2028.ko \
+    $(KERNEL_MODULES_OUT)/tx_macro_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/va_macro_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/wcd938x_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/wcd938x_slave_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/wcd9xxx_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/wcd_core_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/wsa883x_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/wsa_macro_dlkm.ko \
+    $(KERNEL_MODULES_OUT)/xc4000.ko \
+    $(KERNEL_MODULES_OUT)/xc5000.ko
+
+KERNEL_DEFCONFIG := vendor/SAKE-perf_defconfig
+
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x6000000
+BOARD_DTBOIMG_PARTITION_SIZE := 0x1800000
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_KERNEL-GKI_BOOTIMAGE_PARTITION_SIZE := $(BOARD_BOOTIMAGE_PARTITION_SIZE)
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := $(BOARD_BOOTIMAGE_PARTITION_SIZE)
+
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+BOARD_SUPER_PARTITION_SIZE := 7516192768
+BOARD_SUPER_PARTITION_GROUPS := asus_dynamic_partitions
+BOARD_ASUS_DYNAMIC_PARTITIONS_PARTITION_LIST := product system system_ext
+BOARD_ASUS_DYNAMIC_PARTITIONS_SIZE := 7512192768 # BOARD_SUPER_PARTITION_SIZE - 4MB
+
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+
+# Recovery
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.default
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 150
+TARGET_USERIMAGES_USE_F2FS := true
+
+SOONG_CONFIG_NAMESPACES += ufsbsg
+
+SOONG_CONFIG_ufsbsg += ufsframework
+SOONG_CONFIG_ufsbsg_ufsframework := bsg
+
+# VNDK
+BOARD_VNDK_VERSION := current
