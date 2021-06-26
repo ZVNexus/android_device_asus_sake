594eec41f65a85c520a7029913ee8233f82fadee
Alexander Koskovich <zvnexus@outlook.com>
2021-06-26 16:40:21 -0700

    sake: Set DT2W node to "/proc/driver/dclick".
    Change-Id: Iedb4fa76e22ba355dcb9249c93c7e1a245601a85

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,9 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Gestures
+TARGET_TAP_TO_WAKE_NODE := /proc/driver/dclick
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/asus_vendor_framework_compatibility_matrix.xml
