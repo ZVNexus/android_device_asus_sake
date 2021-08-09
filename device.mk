a2484d4eac700b0b59e67c74248d382a7955847b
Alexander Koskovich <zvnexus@outlook.com>
2021-08-09 01:35:24 -0700

    sake: Add proprietary files for system side WFD.
    Change-Id: Ie65a6d76aeb205a863b245c3605ea61ce0885afc

--- device.mk
+++ device.mk
@@ -377,7 +377,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.sake
 
 # WFD
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 PRODUCT_PACKAGES += \
+    libnl \
     libwfdaac_vendor
 
 # WiFi
